40d797677fcbd9e3bb0818f559f27df570bad184
toneiscoding <anthoniduong@gmail.com>
2023-04-19 23:55:33 -0700

    recommit

--- index.js
+++ index.js
@@ -65,34 +65,76 @@ class Shape {
   myCircle3.calculateArea()
   
 // 3. Create a class called Vehicle that has a make and model property and a drive method. Then, create a class called Car that extends Vehicle and has an additional numWheels property and a honk method.
-class Animal {
-    constructor(name) {
-        this.name = name;
+class Vehicle {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
         
     }
  
-    speak() {   
-        console.log('This animal knows how to speak!');
+    drive() {   
+        console.log('This vehicle has driving capabilities');
 
     }
 }
-class Dog extends Animal{
-    constructor(name, breed) {
-        super(name)
-        this.breed = breed;
+class Car extends Vehicle{
+    constructor(make, model, numWheels) {
+        super(make, model)
+        this.numWheels = numWheels;
     }
     
-    bark() {
-        console.log("Arf Arf")
+    Honk() {
+        console.log("Honk Honk")
     }
 }    
 
-let moco = new Dog('moco', 'pug')
-let drax = new Dog( 'drax', 'lucario')
-let bruno = new Dog('bruno','husky')
+let car1 = new Car('Rolls Royce', 'Phantom')
+let car2 = new Car( 'Bentley', 'Continental GT')
+let car3 = new Car('Lamborghini', 'Aventador')
 
-console.log(drax)
+car1.Honk
+car2.Honk
+car3.Honk
+
+
+console.log(car1)
+console.log(car2)
+console.log(car3)
+
+
 // 4. Create a class called Person that has a name and age property and a greet method. Then, create a class called Student that extends Person and has an additional major property and overrides the greet method to include their major.
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        
+        
+    }
+ 
+    greet() {   
+        console.log(`Hello, my name is ${this.name} and my major is ${this.major}`);
+
+    }
+}
+class Student extends Person{
+    constructor(name, age, major) {
+        super(name, age)
+        this.major = major;
+    }
+}    
+
+let student1 = new Student('John', 18, "biology")
+let student2 = new Student( 'Mykel', 20, "construction management")
+let student3 = new Student('Alex', 19, "pyshchology")
+
+student1.greet()
+student2.greet()
+student3.greet()
+
+
+console.log(student1)
+console.log(student2)
+console.log(student3)
 
 
  
@@ -101,6 +143,34 @@ console.log(drax)
 
 
 // 1. Create a factory class called "School" that creates and adds objects from the "Course" class.
+class Course {
+    constructor(name, NOstudents) {
+        this.name = name;
+        this.NOstudents = NOstudents;
+    }
+    logExperience() {
+        console.log(this.name)
+    }
+}
+
+class School {
+    constructor(name, NOstudents){
+        this.name = name;
+        this.NOstudents = NOstudents;
+        this.teachers = [];
+    }
+    addTeacher(name, NOstudents){
+        let newTeacher = new Course(name, NOstudents)
+        this.teachers.push(newTeacher);
+    }
+}
+
+let seattleHigh = new School('Seattle High', 0)
+
+seattleHigh.addTeacher('Chase', 20)
+
+
+
 // 2. Create a factory class called "Portfolio" that creates and adds objects from the "Project" class.
 // 3. Create a factory class called "Company" that creates and adds objects from the "Employee" class.
 class Employee {
